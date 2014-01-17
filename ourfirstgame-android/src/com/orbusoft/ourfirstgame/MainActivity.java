bbe2d1ae7524598e22db4e334e982d20f981ef90
Josh1billion <josh1billion@gmail.com>
2014-01-16 22:21:13 -0600

    Initial project commit
    Contains the libgdx skeleton application.

---
+++ ourfirstgame-android/src/com/orbusoft/ourfirstgame/MainActivity.java
@@ -0,0 +1,18 @@
+package com.orbusoft.ourfirstgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new OurFirstgame(), cfg);
+    }
+}
